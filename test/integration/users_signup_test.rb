9bb5ba9f3ce88fd41f6e93780a233b89d9347be9
watanabe saiya <say-ya111@users.noreply.github.com>
2019-08-03 14:46:53 +0900

    Finish user signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  test "invalid signup information" do
+    get signup_path
+    assert_select 'form[action="/signup"]'
+    assert_no_difference 'User.count' do
+      post signup_path, params: { user: { name: "", email: "user@invalid", password: "foo", password_confirm: "bar" } }
+    end
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert'
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_select 'form[action="/signup"]'
+    assert_difference 'User.count' do
+      post signup_path, params: { user: { name: "Example User", email: "user@example.com", password: "password", password_confirm: "password" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
+
+end
